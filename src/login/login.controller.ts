44666fe6997ed9386ea7a0abdab74dcaa206ded4
Murtaza Pakawala <mypaka52@gmail.com>
2022-08-30 21:47:02 +1000

    the login route setup working

---
+++ src/login/login.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { LoginService } from './login.service';
+
+@Controller('login')
+export class LoginController {
+  constructor(private loginService: LoginService) {}
+
+  @Post()
+  checkLogin(@Body('zid') zid: string, @Body('password') password: string) {
+    return this.loginService.checkLogin(zid, password);
+  }
+}
---
+++ src/login/login.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+
+async function login(zid: string, password: string) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://webcms3.cse.unsw.edu.au/login');
+  //   await page.screenshot({ path: 'amazing1.png' });
+  await page.type('#zid', `${zid}`);
+  await page.type('#password', `${password}`);
+  await page.click('.well button');
+  await page.waitForNavigation();
+  //   await page.screenshot({ path: 'amazing2.png' });
+  const url = await page.url();
+  await browser.close();
+  if (url === 'https://webcms3.cse.unsw.edu.au/login') {
+    return false;
+  } else {
+    return true;
+  }
+}
+@Injectable()
+export class LoginService {
+  //function for checking if user login
+
+  async checkLogin(zid: string, password: string) {
+    return await login(zid, password);
+  }
+}
