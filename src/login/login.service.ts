e5aa8e4b3000ed2cd5dbb8d87e5d6d76f60ed595
Murtaza Pakawala <mypaka52@gmail.com>
2022-09-02 20:53:52 +1000

    minor fix for login endpoint

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -3,20 +3,32 @@ import * as puppeteer from 'puppeteer';
 
 async function loginHelper(zid: string, password: string) {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://webcms3.cse.unsw.edu.au/login');
-  //   await page.screenshot({ path: 'amazing1.png' });
-  await page.type('#zid', `${zid}`);
-  await page.type('#password', `${password}`);
-  await page.click('.well button');
-  await page.waitForNavigation();
-  //   await page.screenshot({ path: 'amazing2.png' });
-  const url = await page.url();
-  await browser.close();
-  if (url === 'https://webcms3.cse.unsw.edu.au/login') {
-    return false;
-  } else {
-    return true;
+  let ans = false;
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://webcms3.cse.unsw.edu.au/login');
+
+    // await page.screenshot({ path: 'amazing1.png' });
+    await page.type('#zid', `${zid}`);
+    await page.type('#password', `${password}`);
+    await page.click('.well button');
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    // await page.screenshot({ path: 'amazing2.png' });
+    const url = await page.url();
+    console.log(url);
+
+    if (url === 'https://webcms3.cse.unsw.edu.au/login') {
+      ans = false;
+    } else {
+      ans = true;
+    }
+    await browser.close();
+    return ans;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await browser.close();
+    return ans;
   }
 }
 @Injectable()
